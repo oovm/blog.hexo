c854363a1782ced1f25d23c4f170e3308376c206
GalAster <280597828@qq.com>
2018-02-22 21:37:26 +0800

    调试::使用旧的 travis 集成方案 放弃 hexo deploy

--- build-tools/!deploy.sh
+++ build-tools/!deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # 不会丢失 commit history 的部署方式
-cd .. && rm .deploy_git -r -f &&
+cd .. && rm .deploy -r -f &&
 hexo clean && hexo g --silent &&
-git clone https://github.com/GalAster/Galaster.github.io .deploy_git &&
-cd .deploy_git && git checkout master &&
-cd ../ && mv .deploy_git/.git/ ./public/ &&
-cd ./public && git add . &&
-git commit -m "Site updated at `date +"%Y-%m-%d %H:%M"` :octocat:" &&
-git push --force https://github.com/GalAster/Galaster.github.io master:master
+mkdir .deploy && cd .deploy &&
+git clone --depth 1 --branch master --single-branch https://github.com/GalAster/Galaster.github.io.git . &&
+rm -rf ./* && cp -r ../public/* . &&
+git add -A . && git commit -m ":octocat:Site updated at  `date +"%Y-%m-%d %H:%M"`." &&
+git branch -m master && git push -q -u origin master
