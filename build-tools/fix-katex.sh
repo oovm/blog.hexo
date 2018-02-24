a44b71d711aca54fdc4809220f933474c167d896
GalAster <galaster@foxmail.com>
2018-02-24 13:34:59 +0800

    微调::样式表更新 微调::变动到hexo分支

--- build-tools/fix-katex.sh
+++ build-tools/fix-katex.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-cd .. &&
-npm uninstall hexo-renderer-marked --save &&
-npm i hexo-renderer-markdown-it-plus --save &&
+cd .. && hexo clean
+npm uninstall hexo-renderer-marked --save
+npm i hexo-renderer-markdown-it-plus --save
 # npm install hexo-math --save &&
-hexo clean
+npm install markdown-it-katex
